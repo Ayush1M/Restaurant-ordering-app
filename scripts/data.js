4e0d52b636a506a24cf92e9cf7b9ee13d889544f
Ayush1M <worklife2442@gmail.com>
2024-02-20 22:48:34 +0530

    added styling for checkout page.

--- scripts/data.js
+++ scripts/data.js
@@ -2,23 +2,23 @@ const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        id: "d8c489cc-8ff3-4521-aff1-9366ebcc172f"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: "8827b7a1-97a7-47aa-b75a-285514db9cb3"
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: "c72a9efe-908b-4152-b586-a35b27bf1932"
     }
 ]
 
