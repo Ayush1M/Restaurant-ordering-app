9345a55e55de379005c5e0c37fde7ac01be58e0a
Ayush1M <worklife2442@gmail.com>
2024-03-02 23:55:38 +0530

    updated the payment content

--- scripts/paymentsummary.js
+++ scripts/paymentsummary.js
@@ -4,6 +4,7 @@ import { getFood } from "./data.js"
 export function paymentSummary(){
     let cartQuantity = 0
     let foodPrice = 0
+    let tax = 0
 
     cart.forEach((cartItem) => {
         cartQuantity += cartItem.quantity
@@ -13,18 +14,24 @@ export function paymentSummary(){
         const foodId = cartItem.foodId
         const matchedItem = getFood(foodId)
         foodPrice += matchedItem.price * cartItem.quantity
+        tax = foodPrice * 0.1
     })
 
     const paymentSummary = `
         <div class="payment-container">
                <h2 class="payment-title">Your order</h2>
            <div class="payment-items">
-               <p>Items(${cartQuantity}) :</p> 
+               <p>Items(${cartQuantity}) :</p>
+                
                <p>$${foodPrice}</p>
+           </div>
+           <div class="tax">
+               <p>Tax(10%) : </p>
+               <p>$${tax.toFixed(2)}</p>
            </div>
            <div class="payment-total">
                <p>Total : </p>
-               <p>$${foodPrice}</p>
+               <p>$${foodPrice + tax}</p>
            </div>
            <button class="payment-btn js-payment-btn">Complete your order</button>
         </div>
